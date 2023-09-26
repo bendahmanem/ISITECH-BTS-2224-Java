8aa7c2fb6ca6efa2332a3ae839985cc5a5e3b9cb
mnbd <mnbdpro@gmail.com>
2023-09-26 17:03:45 +0200

    feat: corrections exo3

--- JavaSwing/JavaCourse/src/Main.java
+++ JavaSwing/JavaCourse/src/Main.java
@@ -1,35 +1,32 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
-        int nombreTentatives = 0;
-        int nb1, nb2, nb3;
-        Random random = new Random();
+        System.out.println("Hello World!");
+        int nombreEssais = 0;
+        int nombreMystere;
+        int nombreEntre = 0;
+        Random rd = new Random();
 
-        // nous allons utiliser la boucle do while
-        // pour que le code s'execute au moins une fois
+        nombreMystere = rd.nextInt(1000);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Veuillez entrer un nombre entre 0 et 1000");
         do {
-            nb1 = random.nextInt(1000);
-            nb2 = random.nextInt(1000);
-            nb3 = random.nextInt(1000);
-
-            nombreTentatives++;
-
-            System.out.println("Tentative n°" + nombreTentatives);
-            System.out.println("Valeur de nb1: " + nb1);
-            System.out.println("Valeur de nb2: " + nb2);
-            System.out.println("Valeur de nb3: " + nb3);
-
-
-            // == et != sont des opérateurs de comparaison
-            // on peut combiner les epxressions booléennes avec les opérateurs logiques
-            // && (et) || (ou) ! (non)
-        } while ( nb1 % 2 != 0 || nb2 % 2 != 0 || nb3 % 2 == 0 );
-
-
-
+            // recuperation de l'entree clavier
+            sc.nextInt();
+            // incrementation du nombre d'essais
+            nombreEssais++;
+            // verification de la valeur entree
+            if (nombreEntre < nombreMystere) {
+                System.out.println("C'est plus !");
+            } else if (nombreEntre > nombreMystere) {
+                System.out.println("C'est moins !");
+            } else {
+                System.out.println("Bravo, vous avez trouve le nombre mystere en " + nombreEssais + " essais !");
+            }
+        } while (nombreEntre != nombreMystere);
     }
 }
