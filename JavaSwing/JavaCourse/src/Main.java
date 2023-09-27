43f8d104f14ad42577f5992aa5a0c9d6f8889b9c
mnbd <mnbdpro@gmail.com>
2023-09-27 16:21:39 +0200

    feat: cours Day3 exos + listes

--- JavaSwing/JavaCourse/src/Main.java
+++ JavaSwing/JavaCourse/src/Main.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
     String nom = new String("Dupont");
+    ArrayList<Livre> liste = new ArrayList<Livre>();
 
+    ListeGenerique<Livre> listeGenerique = new ListeGenerique<Livre>(10);
+    Livre LordOfTheRings = new Livre("L1", "Lord of the Rings", 20.0, 123456789, new Personne("Tolkien", "J.R.R."), 100);
+    listeGenerique.ajout(LordOfTheRings);
+    int nombreDelivres = listeGenerique.getNbElements();
+    System.out.println(listeGenerique.getElement(0).getDesignation());
 
     Article a1 = new Article("A1", "Article 1", 10.0);
     Personne jean = new Personne("Dupont", "Jean");
@@ -12,4 +19,6 @@ public class Main {
 
     }
 }
+
+
 
