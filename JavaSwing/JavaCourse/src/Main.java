0e7a306558037d6c9ca15f6f57f5b03272ba94a8
mnbd <mnbdpro@gmail.com>
2023-09-28 12:01:57 +0200

    feat: cours Day4 introduction aux threads

--- JavaSwing/JavaCourse/src/Main.java
+++ JavaSwing/JavaCourse/src/Main.java
@@ -2,22 +2,43 @@ import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 
-public class Main {
+import javax.swing.*;
 
-    public static void main(String[] args) {
-        JFrame fenetre;
+    public class Main {
+        private JFrame fenetre;
 
-        // Creation de l'instance de la classe JFrame
-        fenetre = new JFrame();
+        public Main(String[] args) {
+            this.fenetre = new JFrame("Jeu de la vie");
+            this.fenetre.setTitle("Jeu de la vie");
+            this.fenetre.setBounds(0, 0, 1000, 1000);
+            this.fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        fenetre.setBounds(0, 0, 1920, 1080);
+            // Creation de boutons
+            JButton b1, b2, b3;
+            b1 = new JButton("Bouton 1");
+            b2 = new JButton("Bouton 2");
+            b3 = new JButton("Bouton 3");
 
-        fenetre.setTitle("Jeu de la vie");
+            // Creation d'un conteneur intermediare
+            JPanel panel = new JPanel();
 
-        fenetre.setVisible(true);
+
+            // Ajout des boutons au conteneur
+            panel.add(b1);
+            panel.add(b2);
+            panel.add(b3);
+
+            this.fenetre.getContentPane().add(panel);
+        }
+
+        public void afficher() {
+            this.fenetre.setVisible(true);
+        }
+
+        public static void main(String[] args) {
+          SwingUtilities.invokeLater(() -> new Main(args).afficher());
+        }
     }
-
-}
 
 
 
