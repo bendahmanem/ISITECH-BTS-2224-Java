f56a3d573361d6bcc7962bbb26fa0988aaebe534
mnbd <mnbdpro@gmail.com>
2023-09-28 10:09:21 +0200

    feat: cours Day4 Errors & Exceptions

--- JavaSwing/JavaCourse/src/Main.java
+++ JavaSwing/JavaCourse/src/Main.java
@@ -1,9 +1,11 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+            public static void main(String[] args) {
 
+/*
     String nom = new String("Dupont");
     ArrayList<Livre> liste = new ArrayList<Livre>();
 
@@ -16,8 +18,48 @@ public class Main {
     Article a1 = new Article("A1", "Article 1", 10.0);
     Personne jean = new Personne("Dupont", "Jean");
     Livre l1 = new Livre("L1", "Livre 1", 20.0, 123456789, jean, 100);
+*/
+    InputStream inputStream = null;
+
+    try {
+        File file =  new File("C:\\Users\\julie\\Documents\\Cours\\Java\\TP\\TP2\\src\\articles.txt");
+
+        inputStream = new FileInputStream(file);
+    } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+    } finally {
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     }
+
+            public static void LireFichier(String nom)
+            {
+                // Premier indice : representez le contenu du fichier ligne par ligne....
+
+                // FileinputStream vous permet de lire le fichier octet par octet... a partir du chemin du fichier...
+
+                // Deuxieme indice : utilisez un BufferedReader pour lire le fichier ligne par ligne
+                // ou
+                // Troisieme indice : utilisez un FileReader pour lire le fichier ligne par ligne
+
+
+
+
+                // Votre code ici
+                // BufferedReader
+                // FileInputStream
+                // InputStreamReader
+                // System.out.println
+                // FileReader
+                // File
+            }
 }
 
 
